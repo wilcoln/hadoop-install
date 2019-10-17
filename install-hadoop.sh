db81550411ed3817a2b8744c14477f9609bf2b56
Wilfried Bounsi <wilcoln99@gmail.com>
2019-10-17 09:39:18 +0200

    Update

--- install-hadoop.sh
+++ install-hadoop.sh
@@ -1,25 +1,26 @@
-# This script asume all nodes of the cluster have a hadoop user
-# which password is common to all of them
-HADOOP_USER_PASSWORD="hadoop"
+#!/usr/bin/env bash
 
 echo "****************************"
 echo "*   Hadoop Cluster Setup   *"
 echo "****************************"
 
-# Adding hosts
+# 0. Loading Variables
+source config.sh
+
+# 1. Adding hosts
 echo "1. Adding all hosts to /etc/hosts ..."
 
 echo $HADOOP_USER_PASSWORD | sudo -S bash -c 'cat conf/hosts >> /etc/hosts'
 
-# Configuring SSH
+# 2. Configuring SSH
 echo "2. Enabling SSH paswordless connection..."
 
-ssh-keygen -t rsa -f ~/.ssh/id_rsa
-HOSTNAMES=`awk '{print $2}' conf/hosts`
+ssh-keygen -t rsa -f ~/.ssh/id_rsa # generate ssh key for the node
+HOSTNAMES=`awk '{print $2}' conf/hosts` # get all hostnames in conf/hosts file
 
 for hostname in $HOSTNAMES
 do
-	ssh-copy-id $hostname
+	ssh-copy-id $hostname # copy node ssh public key to all nodes in the cluster
 done
 
 # Installing Java 8
@@ -33,16 +34,27 @@ echo $HADOOP_USER_PASSWORD | sudo -S apt install openjdk-8-jdk-headless -y
 # Installing Hadoop 3.2.1
 echo "4. Installing Hadoop..."
 
-wget https://www-eu.apache.org/dist/hadoop/common/hadoop-3.2.1/hadoop-3.2.1.tar.gz
-echo $HADOOP_USER_PASSWORD | sudo -S tar -xzf hadoop-3.2.1.tar.gz -C /usr/local/ && rm -rf hadoop-3.2.1.tar.gz
+wget $HADOOP_ORIGIN
+echo $HADOOP_USER_PASSWORD | sudo -S tar -xzf hadoop-3.2.1.tar.gz -C $HADOOP_PARENT_DIR && rm -rf hadoop-3.2.1.tar.gz
 
 # Configuring Hadoop
 echo "5. Configuring Hadoop..."
 
-echo $HADOOP_USER_PASSWORD | sudo -S bash -c 'echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> /usr/local/hadoop-3.2.1/etc/hadoop-env.sh'
-echo $HADOOP_USER_PASSWORD | sudo -S cp conf/hadoop/* /usr/local/hadoop-3.2.1/etc/hadoop/
+echo $HADOOP_USER_PASSWORD | sudo -S bash -c 'source conf/config.sh && echo "export JAVA_HOME=$JAVA_HOME" >> $HADOOP_PARENT_DIR/hadoop-3.2.1/etc/hadoop-env.sh'
+echo $HADOOP_USER_PASSWORD | sudo -S cp conf/hadoop/* $HADOOP_PARENT_DIR/hadoop-3.2.1/etc/hadoop/
 
 # Updating .bashrc
 echo "6. Updating .bashrc..."
 
-echo $HADOOP_USER_PASSWORD | sudo -S bash -c 'cat conf/bashrc >> ~/.bashrc'
+## Add and export Java
+echo $HADOOP_USER_PASSWORD | sudo -S bash -c 'source conf/config.sh && echo "JAVA_HOME=$JAVA_HOME" >> ~/.bashrc'
+echo $HADOOP_USER_PASSWORD | sudo -S bash -c 'source conf/config.sh && echo "export JAVA_HOME" >> ~/.bashrc'
+## Set PSDSH type to ssh
+echo $HADOOP_USER_PASSWORD | sudo -S bash -c 'echo "PDSH_RCMD_TYPE=ssh" >> ~/.bashrc'
+## set Hadoop home directory
+echo $HADOOP_USER_PASSWORD | sudo -S bash -c 'source conf/config.sh && echo "HADOOP_HOME=$HADOOP_PARENT_DIR/hadoop-3.2.1" >> ~/.bashrc'
+## Update and export PATH
+echo $HADOOP_USER_PASSWORD | sudo -S bash -c "source conf/config.sh && echo PATH='$'PATH:'$'HADOOP_HOME/bin:'$'HADOOP_HOME/sbin >> ~/.bashrc"
+echo $HADOOP_USER_PASSWORD | sudo -S bash -c 'source conf/config.sh && echo "export PATH" >> ~/.bashrc'
+
+
--- readme.md
+++ readme.md
@@ -1,10 +1,19 @@
-# Procédure d'installation
-Sur chaque noeud du cluster, faire en tant qu'utilisateur hadoop:
+# Install 
 
 ```
 cd
 wget https://github.com/wilcoln/hadoop-install/archive/master.zip
 unzip master.zip && rm -rf master.zip
 mv hadoop-install-master hadoop-install
-cd hadoop-install && ./install-hadoop.sh
 ```
+
+# Settings
+- Set appropriate variables in `conf/config.sh`
+- Fill in `conf/hosts` with the hostname and IP address of all nodes in the cluster
+- Fill in `conf/hadoop/workers` with the hostname of all the workers in the hadoop cluster
+- Edit all files in `conf/hadoop/` appropriately
+
+# Run
+On all nodes of the cluster, run
+
+`cd hadoop-install && ./install-hadoop.sh`
