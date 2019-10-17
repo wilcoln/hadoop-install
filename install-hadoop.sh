483466dffe0baae97791c8e24d174b4ca59a53f4
Wilfried Bounsi <wilcoln99@gmail.com>
2019-10-17 12:45:32 +0200

    Polish script

--- install-hadoop.sh
+++ install-hadoop.sh
@@ -2,7 +2,7 @@
 
 echo "****************************"
 echo "*   Hadoop Cluster Setup   *"
-echo "****************************"
+echo "****************************\n"
 
 # 0. Loading Variables
 source conf/config.sh
@@ -12,7 +12,7 @@ echo ">>>> 1. Adding all hosts to /etc/hosts ..."
 
 echo $HADOOP_USER_PASSWORD | sudo -S bash -c 'cat conf/hosts >> /etc/hosts'
 
-echo "<<<< done."
+echo "<<<< done. \n"
 
 # 2. Configuring SSH
 echo ">>>> 2. Enabling SSH paswordless connection... <<<<"
@@ -25,7 +25,7 @@ do
 	ssh-copy-id $hostname # copy node ssh public key to all nodes in the cluster
 done
 
-echo "<<<< done."
+echo "<<<< done. \n"
 
 # Installing Java 8
 echo ">>>> 3. Installing Java... <<<<"
@@ -41,7 +41,7 @@ echo ">>>> 4. Installing Hadoop... <<<<"
 wget $HADOOP_ORIGIN
 echo $HADOOP_USER_PASSWORD | sudo -S tar -xzf hadoop-3.2.1.tar.gz -C $HADOOP_PARENT_DIR && rm -rf hadoop-3.2.1.tar.gz
 
-echo "<<<< done."
+echo "<<<< done. \n"
 
 # Configuring Hadoop
 echo ">>>> 5. Configuring Hadoop... <<<<"
@@ -49,7 +49,7 @@ echo ">>>> 5. Configuring Hadoop... <<<<"
 echo $HADOOP_USER_PASSWORD | sudo -S bash -c 'source conf/config.sh && echo "export JAVA_HOME=$JAVA_HOME" >> $HADOOP_PARENT_DIR/hadoop-3.2.1/etc/hadoop-env.sh'
 echo $HADOOP_USER_PASSWORD | sudo -S cp conf/hadoop/* $HADOOP_PARENT_DIR/hadoop-3.2.1/etc/hadoop/
 
-echo "<<<< done."
+echo "<<<< done. \n"
 
 # Updating .bashrc
 echo ">>>> 6. Updating .bashrc... <<<<"
@@ -65,4 +65,4 @@ echo $HADOOP_USER_PASSWORD | sudo -S bash -c 'source conf/config.sh && echo "HAD
 echo $HADOOP_USER_PASSWORD | sudo -S bash -c "source conf/config.sh && echo PATH='$'PATH:'$'HADOOP_HOME/bin:'$'HADOOP_HOME/sbin >> ~/.bashrc"
 echo $HADOOP_USER_PASSWORD | sudo -S bash -c 'source conf/config.sh && echo "export PATH" >> ~/.bashrc'
 
-echo "<<<< done."
+echo "<<<< done. \n"
