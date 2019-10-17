2c93d9cc59dc2a3e783ed944ec716b86e2f4ff6e
Wilfried Bounsi <wilcoln99@gmail.com>
2019-10-17 09:37:50 +0200

    Update

---
+++ conf/config.sh
@@ -0,0 +1,4 @@
+HADOOP_USER_PASSWORD=hadoop
+HADOOP_ORIGIN=https://www-eu.apache.org/dist/hadoop/common/hadoop-3.2.1/hadoop-3.2.1.tar.gz
+JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+HADOOP_PARENT_DIR=/usr/local
